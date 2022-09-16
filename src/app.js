0edf0284ef089d885b45cef7ae7d1c3cdb3a3fdf
Phuong Tran <phuong.tran@ptnglobalcorp.com>
2022-09-16 08:48:17 +0700

    Define routes for managing contacts

--- src/app.js
+++ src/app.js
@@ -1,17 +1,33 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+const contactController = require("./controllers/contactController");
 
 // Settings
 app.use(cors());
 app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json({
-    message: 'Welcome to contact book application. '
+    message: "Welcome to contact book application. ",
   });
 });
+
+app
+  .route(" / api / contacts ")
+  .get(contactController.findA11)
+  .post(contactController.create)
+  .delete(contactController.deleteAll);
+app
+  .route(" / api / contacts / favorite ")
+  .get(contactController.findAllFavorite);
+app
+  .route(" / api / contacts / : id ")
+  .get(contactController.findone)
+  .put(contactController.update)
+  .delete(contactController.delete);
 
 module.exports = app;
